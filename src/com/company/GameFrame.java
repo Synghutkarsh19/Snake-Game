12829279e4c197720dc3bb22694721f1ca8ebc2d
Utkarsh <singhutkarsh1900@gmail.com>
2023-03-30 18:49:07 +0530

    first commit

---
+++ src/com/company/GameFrame.java
@@ -0,0 +1,14 @@
+package com.company;
+import javax.swing.*;
+public class GameFrame extends JFrame {
+    GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Sanke");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+}
